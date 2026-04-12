d4df56b3a2a21ce07266ac06209ee62758bba298
daniel13ros <Danielros883@gmail.com>
2026-04-12 11:18:19 +0300

    added get_date , get_time features

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask , jsonify
+import requests
+import datetime
 
 # Creating the app
 app = Flask(__name__)
@@ -6,7 +8,7 @@ app = Flask(__name__)
 # Using decorator to connect the URL to python
 @app.route('/',methods=['GET'])
 def home():
-    return jsonify({"Message" : "Pokemon API is running"}) # convert dic to json
+    return jsonify({"Message" : "Home page in API is running"}) # convert dic to json
 
 @app.route('/status')
 def status():
@@ -15,6 +17,16 @@ def status():
 @app.route('/hello_name/<name>')
 def hello_name(name):
     return jsonify({"message": f"Hello {name}!"})
+
+@app.route('/get_date')
+def get_date():
+    current_date = datetime.datetime.now().strftime("%Y-%m-%d")
+    return jsonify({"date": current_date})
+
+@app.route('/get_time')
+def get_time():
+    current_time = datetime.datetime.now().strftime("%H:%M:%S")
+    return jsonify({"time": current_time})
 
 if __name__ == '__main__':
     app.run(debug=True,port=5000)
