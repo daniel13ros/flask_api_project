eb31bb678f2a3720ba461ef030a8a7aaf77ba53d
daniel13ros <Danielros883@gmail.com>
2026-04-13 14:01:11 +0300

    first api app , fixed app.route methodes

--- first api app/app.py
+++ first api app/app.py
@@ -11,20 +11,20 @@ app = Flask(__name__)
 def home():
     return jsonify({"Message" : "Home page in API is running"}) # convert dic to json
 
-@app.route('/status')
+@app.route('/status' , methods=['GET'])
 def status():
     return jsonify({"Version":"1.0.0","status":"healthy"})
 
-@app.route('/hello_name/<name>')
+@app.route('/hello_name/<name>' , methods=['GET'])
 def hello_name(name):
     return jsonify({"message": f"Hello {name}!"})
 
-@app.route('/get_date')
+@app.route('/get_date' , methods=['GET'])
 def get_date():
     current_date = datetime.datetime.now().strftime("%Y-%m-%d")  
     return jsonify({"date": current_date})
 
-@app.route('/get_time')
+@app.route('/get_time' , methods=['GET'])
 def get_time():
     current_time = datetime.datetime.now().strftime("%H:%M:%S")
     return jsonify({"time": current_time})
